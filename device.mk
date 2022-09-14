27a829875c3bedc915e719de2cdf06d07ffd9cc4
bengris32 <bengris32@protonmail.ch>
2022-09-14 19:14:53 +0100

    cezanne: Build required radio interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Iaa8ceef6a0529597580d8ff6d7bf9773cb62b394

--- device.mk
+++ device.mk
@@ -197,6 +197,20 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0.vendor \
+    android.hardware.radio@1.1.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.3.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio-V1.4-java
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6885 \
