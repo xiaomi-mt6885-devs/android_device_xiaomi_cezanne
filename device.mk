9afcb93d96ceb07363a07beec434f94f7e91006f
bengris32 <bengris32@protonmail.ch>
2023-01-05 15:11:01 +0100

    cezanne: Build required DRM interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I5bbf17c83c7f32cf8c41d26303c371a6eb5acca0

--- device.mk
+++ device.mk
@@ -57,6 +57,13 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5.vendor \
     android.hardware.camera.provider@2.6.vendor
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
