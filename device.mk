d501ea6f0761a8db62a864767b7dbb90d7daef22
Matsvei Niaverau <matvej2002@gmail.com>
2023-01-05 14:42:30 +0100

    cezanne: configs: Import public libraries list from stock
    Change-Id: I9e0087ce1d40f5eafda0426d1440980e1a93fae1

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6885 \
