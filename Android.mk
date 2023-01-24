c6fd15bf7e6bb15841e6000442c076cb8a73e608
AgentFabulous <kshitijgm@gmail.com>
2023-01-25 04:37:37 +0800

    cezanne: Add vendor symlinks

--- Android.mk
+++ Android.mk
@@ -8,15 +8,21 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),cezanne)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(CLEAR_VARS)
 
-VULKAN_SYMLINKS := $(TARGET_OUT_VENDOR)
-$(VULKAN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating vulkan symlinks: $@"
-	@mkdir -p $@/lib/hw/
-	@mkdir -p $@/lib64/hw/
-	@ln -sf $@/lib/egl/libGLES_mali.so $@/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-	@ln -sf $@/lib64/egl/libGLES_mali.so $@/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+VENDOR_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw
 
-ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINKS)
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6885.so
+	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6885.so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
 
 endif
