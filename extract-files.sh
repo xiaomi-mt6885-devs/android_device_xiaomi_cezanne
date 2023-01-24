f94fe4ee5dd162b72a4c97256b300c9c00282076
SamarV-121 <samarvispute121@pm.me>
2023-01-25 06:46:14 +0800

    cezanne: Link PQ and dfps HAL against VNDK 32 libutils
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    [bengris32] Patch both 32bit and 64bit variants

--- extract-files.sh
+++ extract-files.sh
@@ -83,6 +83,9 @@ function blob_fixup() {
         vendor/lib64/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
+        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.6-impl.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
