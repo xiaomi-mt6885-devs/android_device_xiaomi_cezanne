b98d7f75f184d32bcf98c58598fd2b44fe9429a0
AngelaCooljx <cool@gayhub.eu.org>
2023-01-25 02:03:44 +0800

    cezanne: Kang ImsService from ares MIUI-14
    `missi_phoneext4_cn-user 13 TP1A.220624.014 V14.0.22.12.5.DEV release-keys `

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
+# ImsService
+PRODUCT_PACKAGES += \
+    ImsService
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
