9329c12b5d023a2cd20068a90df8953893b18eb3
AbhiShek Aggarwal <warabhishek@gmail.com>
2023-01-25 15:37:13 +0800

    cezanne: Add custom init to set proper variants
    Co-authored-by: Zinadin Zidan <zidan.roking@gmail.com>
    Co-authored-by: raghavt20 <raghavtandon2000@gmail.com>

--- init/init.cpp
+++ init/init.cpp
@@ -1,48 +1,82 @@
 /*
- * Copyright (C) 2020 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+   Copyright (c) 2015, The Linux Foundation. All rights reserved.
+   Copyright (C) 2016 The CyanogenMod Project.
+   Copyright (C) 2019-2020 The LineageOS Project.
+   Copyright (C) 2021 Paranoid Android.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cstring>
-#include <sys/sysinfo.h>
-
+#include <stdlib.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
+#include <android-base/properties.h>
 #include "property_service.h"
+#include "vendor_init.h"
 
-void property_override(char const prop[], char const value[])
+using android::base::GetProperty;
+using android::base::SetProperty;
+using std::string;
+
+void property_override(string prop, string value)
 {
-    auto pi = (prop_info *) __system_property_find(prop);
+    auto pi = (prop_info*) __system_property_find(prop.c_str());
 
-    if (pi != nullptr) {
-        __system_property_update(pi, value, strlen(value));
-    } else {
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-    }
+    if (pi != nullptr)
+        __system_property_update(pi, value.c_str(), value.size());
+    else
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
 }
 
-void load_dalvik_properties()
+void vendor_load_properties()
 {
-    char const *heapstartsize;
-    char const *heapgrowthlimit;
-    char const *heapsize;
-    char const *heapminfree;
-    char const *heapmaxfree;
-    char const *heaptargetutilization;
-    struct sysinfo sys;
+    string device, model, marketname;
 
+    device = "cezanne";
+    model = "M2006J10C";
+    marketname = "Redmi K30 Ultra";
+
+    // Override all partitions' props
+    string prop_partitions[] = { "", "bootimage.", "odm.", "product.", "system.", "system_ext.", "vendor." };
+    for (const string &prop : prop_partitions) {
+        property_override(string("ro.product.") + prop + string("board"), device);
+        property_override(string("ro.product.") + prop + string("device"), device);
+        property_override(string("ro.product.") + prop + string("name"), device);
+        property_override(string("ro.product.") + prop + string("model"), model);
+        property_override(string("ro.") + prop + string("build.product"), device);
+    }
+
+    // Set hardware SKU prop
+    property_override("ro.boot.product.hardware.sku", device);
+
+    // Set dalvik heap configuration
+    string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
+			heapmaxfree, heaptargetutilization;
+
+    struct sysinfo sys;
     sysinfo(&sys);
 
     if (sys.totalram >= 7ull * 1024 * 1024 * 1024) {
@@ -69,9 +103,4 @@ void load_dalvik_properties()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
-}
-
-void vendor_load_properties()
-{
-    load_dalvik_properties();
 }
